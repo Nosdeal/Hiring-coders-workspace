958ef56a61d0f1eb7c60afd00c557a624ead7f94
Nosdeal <laedson038@gmail.com>
2022-05-04 08:56:12 -0300

    Novos arquivos

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//*let sorvete = prompt("Qual seu sorvete favorito?")
+//* let res = document.querySelector('sorvete')
+//*if (res == 'Chocolate'){
+//*    alert('Hmmm, meu favoritoo!')
+//*}else{
+   //* alert('Prefiro chocolate!')
+//*}
+
+let num1 = prompt("Digite um número")
+let num2 = prompt("Digite outro número")
+let soma = Number(num1) + Number(num2)
+alert("A soma de " +num1+ " e " +num2+ " é igual a " +soma)
+document.querySelector('html').onclick = function() {
+    alert('Ai! Pare de me cutucar!');
+}
